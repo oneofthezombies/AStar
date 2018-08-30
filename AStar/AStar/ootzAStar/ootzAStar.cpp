9f781eafc4a8255fb13d924f759beaa18912138f
oneofthezombies <hunhoekim@gmail.com>
2018-08-30 10:49:55 +0900

    add: project setting, dx11 setting

---
+++ AStar/AStar/ootzAStar/ootzAStar.cpp
@@ -0,0 +1,8 @@
+#include "stdafx.h"
+#include "ootzAStar.h"
+
+namespace ootz
+{
+    
+
+} // namespace ootz
---
+++ AStar/AStar/ootzAStar/ootzAStar.h
@@ -0,0 +1,17 @@
+#pragma once
+
+
+
+namespace ootz
+{
+
+using Float3 = DirectX::XMFLOAT3;
+
+class AStar
+{
+
+};
+
+
+
+} // namespace ootz
